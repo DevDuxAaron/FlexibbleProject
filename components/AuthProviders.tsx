4be7e7081ed968805ea0fffff8d77c21699493dc
Aaron Joel <relojvale25@gmail.com>
2023-07-28 21:07:48 -0400

    Created session and actions

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+"use client"
+
+import { BuiltInProviderType } from 'next-auth/providers'
+import { ClientSafeProvider, LiteralUnion, getProviders, signIn } from 'next-auth/react'
+import { useState, useEffect } from 'react'
+
+type Provider = {
+  id: string,
+  name: string,
+  type: string,
+  signinUrl: string,
+  callBackUrl: string,
+  signinUrlParams: Record<string, string> | null
+}
+
+type Providers = Record<string, Provider> | Promise<Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null>
 
 const AuthProviders = () => {
-  return (
-    <div>AuthProviders</div>
-  )
+  const [providers, setProviders] = useState<Providers| Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null>(null)
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders()
+      // console.log(res);
+
+      setProviders(res)
+    }
+
+    fetchProviders()
+  }, [])
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, i)=> <button key={i}>{provider.id}</button>)}
+      </div>
+    )
+  }
 }
 
 export default AuthProviders
